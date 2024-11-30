8d5ccbe197c38230139a8426c83a23f9924a9293
ViooWA <vioobot@gmail.com>
2024-11-30 15:27:11 +0700

    Add files via upload

--- api.js
+++ api.js
@@ -122,45 +122,53 @@ return imageUrl;
 }
 
 async function remini(imageData, action) {
-return new Promise(async (resolve, reject) => {
 let actions = ['enhance', 'recolor', 'dehaze'];
-if (!actions.includes(action)) {
-action = actions[0];
-}
-let formData = new FormData();
-let url = `https://inferenceengine.vyro.ai/${action}`;
-formData.append('model_version', 1, {
-'Content-Transfer-Encoding': 'binary',
-'contentType': 'multipart/form-data; charset=uttf-8'
-});
-formData.append('image', Buffer.from(imageData), {
-'filename': 'enhance_image_body.jpg',
-'contentType': 'image/jpeg'
-});
-formData.submit({
-'url': url,
-'host': 'inferenceengine.vyro.ai',
-'path': `/${action}`,
-'protocol': 'https:',
-'headers': {
+if (!actions.includes(action)) action = 'enhance';
+const url = `https://inferenceengine.vyro.ai/${action}`;
+const formData = new FormData();
+formData.append('model_version', '1');
+formData.append('image', imageData, 'enhance_image_body.jpg');
+const response = await axios.post(url, formData, {
+headers: {
+...formData.getHeaders(),
 'User-Agent': 'okhttp/4.9.3',
-'Connection': 'Keep-Alive',
-'Accept-Encoding': 'gzip'
-}}, function (err, res) {
-if (err) {
-reject();
-return; }
-let chunks = [];
-res.on('data', chunk => {
-chunks.push(chunk);
-}).on('end', () => {
-resolve(Buffer.concat(chunks));
-}).on('error', () => {
-reject();
-});
-});
-});
+}, responseType: 'arraybuffer', });
+return response.data;
 }
+
+const api = axios.create({
+baseURL: 'https://api4g.iloveimg.com'
+})
+const getTaskId = async () => {
+const {
+data: html
+} = await axios.get('https://www.iloveimg.com/id/hapus-latar-belakang')
+api.defaults.headers.post['authorization'] = `Bearer ${html.match(/ey[a-zA-Z0-9?%-_/]+/g)[1]}`
+return html.match(/taskId = '(\w+)/)[1]
+}
+
+const uploadImageToServer = async (imageBuffer) => {
+const taskId = await getTaskId()
+const fileName = Math.random().toString(36).slice(2) + '.jpg'
+const form = new FormData()
+form.append('name', fileName)
+form.append('chunk', '0')
+form.append('chunks', '1')
+form.append('task', taskId)
+form.append('preview', '1')
+form.append('pdfinfo', '0')
+form.append('pdfforms', '0')
+form.append('pdfresetforms', '0')
+form.append('v', 'web.0')
+form.append('file', imageBuffer, fileName)
+const reqUpload = await api.post('/v1/upload', form, {
+headers: form.getHeaders()
+}).catch(e => e.response)
+if (reqUpload.status !== 200) throw reqUpload.data || reqUpload.statusText
+return {
+serverFilename: reqUpload.data.server_filename,
+taskId
+}}
 
 const removeBg = async (imageBuffer, responseType = 'arraybuffer') => {
 const {
@@ -718,8 +726,7 @@ const resultImage = await remini(imageData, 'dehaze');
 res.setHeader('Content-Type', 'image/jpeg');
 res.send(resultImage);
 } else if (s === 'ssweb') { // SSWEB
-const response = await axios.get(
-`https://api.siputzx.my.id/api/tools/ssweb?url=${url}&theme=light&device=desktop`,
+const response = await axios.get(`https://api.vreden.my.id/api/ssweb?url=${url}&type=tablet`,
 { responseType: 'arraybuffer' }
 );
 res.setHeader('Content-Type', 'image/png');
